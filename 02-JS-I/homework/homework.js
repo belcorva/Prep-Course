d209aa1549259bda61831a413cd2739790b251b4
belcorva <belcorvalan@hotmail.com>
2022-06-12 11:51:23 -0300

    terceraprueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -25,7 +25,7 @@ const nuevoModulo = 21 % 5 === 1;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString('str') {
+function devolverString (str) {
   return (devolverString.length);
   // "Return" la string provista: str
   // Tu código:
@@ -63,6 +63,10 @@ function divide(x, y) {
 }
 
 function sonIguales(x, y) {
+  if(x==y) { 
+    return true;  
+  }
+  return false;
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
@@ -70,6 +74,10 @@ function sonIguales(x, y) {
 }
 
 function tienenMismaLongitud(str1, str2) {
+  if (str1 == str2) {
+    return true;
+  }
+  return false;
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
@@ -77,6 +85,10 @@ function tienenMismaLongitud(str1, str2) {
 }
 
 function menosQueNoventa(num) {
+  if (num < 90) {
+    return true;
+  }
+  return false;
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
